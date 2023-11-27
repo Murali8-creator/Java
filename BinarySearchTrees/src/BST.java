f99bd7a5c3e377a032ff109aed95c7fe1373f3a5
Murali8-creator <muralig436@gmail.com>
2023-11-27 23:33:59 +0530

    added preOrder,postOrder and inOrder methods

--- BinarySearchTrees/src/BST.java
+++ BinarySearchTrees/src/BST.java
@@ -63,4 +63,42 @@ public class BST {
 
     }
 
+    public void preOrder(){
+        preOrder(root);
+    }
+    private void preOrder(Node node){
+        if (node==null)
+            return;
+        System.out.print(node.value+" ");
+
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    public void postOrder(){
+        postOrder(root);
+    }
+    private void postOrder(Node node){
+        if (node==null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.print(node.value+" ");
+    }
+
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    public void inOrder(Node node){
+        if (node==null)
+            return;
+
+        inOrder(node.left);
+        System.out.print(node.value+" ");
+        inOrder(node.right);
+    }
+
+
 }
--- BinarySearchTrees/src/Main.java
+++ BinarySearchTrees/src/Main.java
@@ -2,11 +2,20 @@ public class Main {
     public static void main(String[] args) {
         BST bst=new BST();
 
+        bst.insert(15);
         bst.insert(10);
+        bst.insert(20);
+        bst.insert(5);
+        bst.insert(12);
         bst.insert(8);
-        bst.insert(17);
+        bst.insert(19);
 
         bst.display();
+        bst.preOrder();
+        System.out.println();
+        bst.postOrder();
+        System.out.println();
+        bst.inOrder();
 
 
 
